8c4dba0cc9ae58190a31e30141329109efdf5953
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2021-05-25 08:18:49 -0300

    Criando as páginas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+
+import { IFilme } from '../model/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -12,42 +14,50 @@ import { ToastController } from '@ionic/angular';
 
 export class Tab1Page {
 
-  public listaFilmes = [
+  public listaDeFilmes: IFilme[] = [
     {
-      "nome":'Tom & Jerry - O Filme (2021)',
-      "img":'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
-      "genero":'Comédia, Família, Animação',
-      "data":'11/02/2021 (BR) - 1h 41m',
-      "gostou":"73%"
+      nome:'Tom & Jerry - O Filme (2021)',
+      lancamento:'11/02/2021 (BR)',
+      duracao:'1h 41m',
+      classificacao:73,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos:['Comédia', 'Família', 'Animação'],
     },
     {
-      "nome":'Sem Remorso (2021)',
-      "img":'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
-      "genero":'Ação, Aventura, Thriller, Guerra',
-      "data":'30/04/2021 (BR) - 1h 50m',
-      "gostou":"78%"
+      nome:'Sem Remorso (2021)',
+      lancamento:'30/04/2021 (BR)',
+      duracao:'1h 50m',
+      classificacao:80,
+      cartaz:'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos:['Ação', 'Aventura', 'Thriller', 'Guerra'],
+      pagina:'/sem-remorso'
     },
     {
-      "nome":'O Conto da Aia (2017)',
-      "img":'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2rK53k6Lg6IDqHM7xp8OTzzzpz7.jpg',
-      "genero":'Sci-Fi & Fantasy, Drama',
-      "data":'30/04/2017 (BR) - 1h 50m',
-      "gostou":"82%"
+      nome:'O Conto da Aia (2017)',
+      lancamento:'30/04/2017 (BR)',
+      duracao:'1h 50m',
+      classificacao:78,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2rK53k6Lg6IDqHM7xp8OTzzzpz7.jpg',
+      generos:['Sci-Fi & Fantasy', 'Drama'],
     },
     {
-      "nome":'Três Homens em Conflito (1966)',
-      "img":'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f6CoXpyZRqYJF3lTfxjWIahO6qM.jpg',
-      "genero":'Faroeste',
-      "data":'11/01/1968 (BR) - 2h 58m',
-      "gostou":'85%'
+      nome:'Três Homens em Conflito (1966)',
+      lancamento:'11/01/1968 (BR)',
+      duracao:'2h 58m',
+      classificacao:82,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f6CoXpyZRqYJF3lTfxjWIahO6qM.jpg',
+      generos:['Faroeste'],
     },
     {
-      "nome":'Mortal Kombat (2021)',
-      "img":'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
-      "genero":'Ação, Fantasia, Aventura',
-      "data":'15/04/2021 (BR) - 1h 50m',
-      "gostou":'76%'
-    },
+      nome:'Mortal Kombat (2021)',
+      lancamento:'15/04/2021 (BR)',
+      duracao:'1h 50m',
+      classificacao:78,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+      generos:['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat'
+    }
+
   ];
 
   constructor(
